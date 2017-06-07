39de916a3c27aa4fc9f2bb8907ee6ef5ed0d4e45
Ai Nguyen <ainguyen01@users.noreply.github.com>
2017-06-07 13:39:38 -0500

    slurpt

---
+++ db/migrate/20170607183324_create_requests.rb
@@ -0,0 +1,11 @@
+class CreateRequests < ActiveRecord::Migration[5.0]
+  def change
+    create_table :requests do |t|
+      t.string :drug
+      t.integer :user_id
+
+      t.timestamps
+
+    end
+  end
+end
