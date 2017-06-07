f7e2425c76c909060f9d938302d8f12fde767f9a
Ai Nguyen <ainguyen01@users.noreply.github.com>
2017-06-07 14:12:26 -0500

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         has_many :requests, :dependent => :destroy
+         has_many :dislikes, :dependent => :destroy
+         has_many :comments, :dependent => :destroy
+         has_many :copay_cards, :class_name => "Copay"
 end
